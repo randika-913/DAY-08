88da664eb7f3a7b4dfcd0dbb97c041e7031b449f
randika-913 <randikasampath2020@gmail.com>
2026-08-06 19:28:27 +0530

    new 1

--- app.js
+++ app.js
@@ -48,20 +48,6 @@ fetch(`${baseUrl}/Current.json?key=${apiKey}&q=panadura`).then(res=>res.json()).
     `
 })
 
-fetch(`${baseUrl}/Current.json?key=${apiKey}&q=panadura`).then(res=>res.json()).then(data=>{
-
-    document.getElementById("contentSection").innerHTML=`
-        <div>
-            <h1>${data.current.condition.text}</h1>
-            <h1>${data.location.name}</h1>
-            <img src="${data.current.condition.icon}" alt="">
-            <p>${data.location.country}</p>
-            <p>${data.current.temp_c}</p>
-        </div>
-
-    `
-})
-
 function btnSearchOnAction(){
     let txtUserSearchValue = document.getElementById("txtSearch").value;
     fetch(`${baseUrl}/current.json?key=${apiKey}&q=${txtUserSearchValue}`).then(res=>res.json()).then(data=>{
